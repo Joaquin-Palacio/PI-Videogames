47f0747cd859091394a8b0cbba6428b953dd1f16
Joaquin Palacio <joaquinpalacio2311@gmail.com>
2022-09-12 11:24:24 -0300

    style detail finish

--- client/src/components/CardDetail.jsx
+++ client/src/components/CardDetail.jsx
@@ -15,66 +15,75 @@ export default function CardDetail() {
   const videogame = useSelector((state) => state.detail);
 
   return (
-    <div className="detail_component">
-      <div className="allCard">
-        {videogame.name ? (
-          <div className="cardDetail">
-            <div className="containterLeft">
-              <h1>{videogame.name}</h1>
-              <img
-                src={videogame.image}
-                className="imgDetailCard"
-                alt="Imagen del jueego"
-              />
-              <br></br>
-              <h3>Description</h3>
-              <p>{videogame.description}</p>
-              <h3>Genres:</h3>
-              {videogame.genres?.map((e) => {
-                if (typeof e === "string") {
-                  return (
-                    <span className="type" key={e}>
-                      {e.replace(e[0], e[0].toUpperCase())} |
-                    </span>
-                  );
-                } else {
-                  return <span key={e.name}>{e.name} |</span>;
-                }
-              })}
-              <h3>Platforms:</h3>
-              {videogame.platforms?.map((e) => {
-                if (typeof e === "string") {
-                  return (
-                    <span className="type" key={e}>
-                      {e.replace(e[0], e[0].toUpperCase())} |
-                    </span>
-                  );
-                } else {
-                  return <span key={e.name}>{e.name} |</span>;
-                }
-              })}
-            </div>
-            <div className="containterRight_detail">
-              <Link to="/home">
-                <button className="ButtonBack">Back to home!</button>
-              </Link>
-              <div className="info">
-                <label>Released</label>
-                <h6>{videogame.released}</h6>
-                <label>Rating</label>
-                <h3>{videogame.rating}</h3>
-              </div>
-            </div>
+    <div className="container-detail">
+      {videogame.name ? (
+        <div className="detail-card">
+          <h1>{videogame.name}</h1>
+          <img
+            src={videogame.image}
+            className="detail-image"
+            alt="Video game image"
+          />
+          <br />
+          <div className="detail-description">
+            <h3>Description</h3>
+            <p>{videogame.description}</p>
           </div>
-        ) : (
-          <div className="loading">
-            <img
-              src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/04de2e31234507.564a1d23645bf.gif"
-              alt=""
-            />
+          <div className="detail-released">
+            <h4>Released</h4>
+            <p>{videogame.released}</p>
           </div>
-        )}
-      </div>
+          <div className="detail-rating">
+            <h4>Rating</h4>
+            <p>{videogame.rating}</p>
+          </div>
+          <br />
+          <Link to="/home">
+            <button className="detail-btn-back">Back Home</button>
+          </Link>
+          <div className="detail-gen-plat">
+            <h3>Genres:</h3>
+            {videogame.genres?.map((e) => {
+              if (typeof e === "string") {
+                return (
+                  <span className="detail-span" key={e}>
+                    {e.replace(e[0], e[0].toUpperCase())} |
+                  </span>
+                );
+              } else {
+                return (
+                  <span className="detail-span" key={e.name}>
+                    {e.name} |
+                  </span>
+                );
+              }
+            })}
+            <h3>Platforms:</h3>
+            {videogame.platforms?.map((e) => {
+              if (typeof e === "string") {
+                return (
+                  <span className="detail-span" key={e}>
+                    {e.replace(e[0], e[0].toUpperCase())} |
+                  </span>
+                );
+              } else {
+                return (
+                  <span className="detail-span" key={e.name}>
+                    {e.name} |
+                  </span>
+                );
+              }
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="loading">
+          <img
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/04de2e31234507.564a1d23645bf.gif"
+            alt=""
+          />
+        </div>
+      )}
     </div>
   );
 }
