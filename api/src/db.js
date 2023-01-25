f49f50ada8233f8a7b868000ef27c64d0de627f3
Joaquin <joaquinpalacio2311@gmail.com>
2023-01-25 18:58:44 -0300

    cambios para deploy

--- api/src/db.js
+++ api/src/db.js
@@ -1,55 +1,35 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOYMENT } = process.env;
+/* 
+var sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+  {
+    logging: false,
+    native: false,
+  }
+); */
 
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-        { logging: false, native: false }
-      );
+var sequelize = new Sequelize(DB_DEPLOYMENT, {
+  logging: false,
+  native: false,
+});
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, "/models"))
+fs.readdirSync(path.join(__dirname, '/models'))
   .filter(
     (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
@@ -69,10 +49,10 @@ const { Videogame, Genre, Platform } = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
-Videogame.belongsToMany(Genre, { through: "videogame_genre" });
-Genre.belongsToMany(Videogame, { through: "videogame_genre" });
-Videogame.belongsToMany(Platform, { through: "videogame_platforms" });
-Platform.belongsToMany(Videogame, { through: "videogame_platforms" });
+Videogame.belongsToMany(Genre, { through: 'videogame_genre' });
+Genre.belongsToMany(Videogame, { through: 'videogame_genre' });
+Videogame.belongsToMany(Platform, { through: 'videogame_platforms' });
+Platform.belongsToMany(Videogame, { through: 'videogame_platforms' });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
